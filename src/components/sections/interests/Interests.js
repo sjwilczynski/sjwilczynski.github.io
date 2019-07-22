ef37cd0634a6001cebe50fa11c3d333abbce8c76
sjwilczynski <sjwilczynski@gmail.com>
2019-07-22 18:15:07 +0200

    add button

--- src/components/sections/interests/Interests.js
+++ src/components/sections/interests/Interests.js
@@ -1,18 +1,30 @@
 import React, {Component} from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import concertDatas from './../../../data/concerts';
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import {Concert} from "../../../data/wrappers/Concert";
+import Button from "react-bootstrap/Button";
+import {scroller} from "react-scroll";
+import './interests.css'
 
 class Interests extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.concertsList = concertDatas.map(concertData => new Concert(concertData));
-        this.concertsList.sort((c1, c2) => c2.startDate - c1.startDate);
+        this.state = {
+            showConcerts: false,
+            concertsList : concertDatas.map(concertData => new Concert(concertData))
+        }
+        this.state.concertsList.sort((c1, c2) => c2.startDate - c1.startDate);
     }
 
+    onConcertsClick = () => {
+        this.setState((state, props) => ({
+            showConcerts: !state.showConcerts
+        }));
+        scroller.scrollTo('interests')
+    };
 
     render() {
         return (
@@ -28,22 +40,27 @@ class Interests extends Component {
                     href="https://www.youtube.com/watch?v=xG91krXuxyw">world record</a>.
                 </p>
                 <p>
-                    As for my other interests I am keen on travelling <FontAwesomeIcon icon={"plane"}/> (most beautiful city - Porto,
+                    As for my other interests I am keen on travelling <FontAwesomeIcon icon={"plane"}/> (most beautiful
+                    city - Porto,
                     Portugal, most astonishing view - Tatev, Armenia, best beach -
                     Tangalle, Sri Lanka). Aside from that, in my free time I like visiting escape rooms and
                     reading books <FontAwesomeIcon icon={"book"}/> (particularly fantasy
                     and history). However my most beloved activity is going to rock
                     concerts (I've been to around 40 already).
+                    <Button className='mt-2 btn-concerts' block onClick={this.onConcertsClick}> Click here
+                        to {this.state.showConcerts ? 'hide' : 'view'} the full concerts list </Button>
                 </p>
-                <VerticalTimeline>
 
-                    {this.concertsList.map(concert =>
-                    <VerticalTimelineElement date={concert.getDate()}>
-                        <h3>{concert.title}</h3>
-                        <h4>{concert.location}</h4>
-                        <p>{concert.description}</p>
-                    </VerticalTimelineElement>)}
+                {this.state.showConcerts &&
+                <VerticalTimeline>
+                    {this.state.concertsList.map(concert =>
+                        <VerticalTimelineElement key={concert.id} date={concert.getDate()}>
+                            <h3>{concert.title}</h3>
+                            <h4>{concert.location}</h4>
+                            <p>{concert.description}</p>
+                        </VerticalTimelineElement>)}
                 </VerticalTimeline>
+                }
             </>
         );
     }
