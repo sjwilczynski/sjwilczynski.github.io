a4462404c3b7b52a0aa329db0f16b55b6cc1af40
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-29 17:42:03 +0000

    Deploy sjwilczynski/sjwilczynski.github.io to github.com/sjwilczynski/sjwilczynski.github.io.git:master

---
+++ precache-manifest.d5a12d46d3d61ace7857966170227019.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dff399d187c618e3f02605c90636f2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0e571bfffe47bcbb599",
+    "url": "/static/css/2.72029c10.chunk.css"
+  },
+  {
+    "revision": "73e3c1d5293a8f052c9a",
+    "url": "/static/css/main.ad27135b.chunk.css"
+  },
+  {
+    "revision": "d0e571bfffe47bcbb599",
+    "url": "/static/js/2.30ac8130.chunk.js"
+  },
+  {
+    "revision": "118fc8a7adbcb6f772992e7d12fb1084",
+    "url": "/static/js/2.30ac8130.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73e3c1d5293a8f052c9a",
+    "url": "/static/js/main.61cf8588.chunk.js"
+  },
+  {
+    "revision": "0393515aea584b170250",
+    "url": "/static/js/runtime-main.9f72f2f7.js"
+  },
+  {
+    "revision": "4637069b49186fd44bcee73f827243cd",
+    "url": "/static/media/face.4637069b.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0884024323d4ad6cfd88deb482124f17.js"
+  "/precache-manifest.d5a12d46d3d61ace7857966170227019.js"
 );
 
 self.addEventListener('message', (event) => {
