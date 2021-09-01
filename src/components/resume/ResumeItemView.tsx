a33e891f519f44fb823a22e0331531cda7486fdd
sjwilczynski <sjwilczynski@gmail.com>
2021-09-01 21:00:24 +0200

    fix horizontal alignment

--- src/components/resume/ResumeItemView.tsx
+++ src/components/resume/ResumeItemView.tsx
@@ -7,7 +7,7 @@ export default function ResumeItemView(props: ResumeItem) {
   return (
     <>
       <div className="resume-item d-flex flex-column flex-md-row mb-5">
-        <div className="resume-content mr-auto">
+        <div className="resume-content me-auto">
           {headings.map((heading) => (
             <h3 key={heading} className="mb-0">
               {heading}
@@ -16,7 +16,7 @@ export default function ResumeItemView(props: ResumeItem) {
           <div className="subheading mb-3">{subheading}</div>
           <ReactMarkdown>{description}</ReactMarkdown>
         </div>
-        <div className="resume-extra text-md-right">
+        <div className="resume-extra text-md-end">
           {extraInfos.map((info) => (
             <span key={info} className="text-primary">
               {info} <br />{" "}
