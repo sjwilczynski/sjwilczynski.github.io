ceac11525cec59f97288391a9b9e48694b5e4cdb
sjwilczynski <sjwilczynski@gmail.com>
2022-09-03 13:52:08 +0200

    remove 85% of unused css

---
+++ postcss.config.js
@@ -0,0 +1,23 @@
+const purgecss = require("@fullhuman/postcss-purgecss");
+
+module.exports = {
+  plugins: [
+    purgecss({
+      content: [
+        "src/**/*.astro",
+        "src/**/*.tsx",
+        "src/data/**/*.json",
+        "node_modules/react-vertical-timeline-component/**/*.js",
+        "node_modules/@fortawesome/fontawesome-svg-core/**/*.js",
+      ],
+      css: [
+        "src/**/*.scss",
+        "node_modules/react-vertical-timeline-component/style.min.css",
+        "node_modules/@fortawesome/fontawesome-svg-core/styles.css",
+      ],
+      safelist: ["active", "collapsing"], // Some class names are listed here as they correspond to dynamically added styles.
+      // active - added to make sure active links styling from bootstrap navitems is not removed.
+      // collapsing - added to make sure the collapsing/expanding animation from bootstrap navigation toggle is not removed.
+    }),
+  ],
+};
