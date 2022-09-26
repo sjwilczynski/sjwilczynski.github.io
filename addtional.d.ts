4fa76117b2fea8312400cd432b9700f0dfcf2a15
sjwilczynski <sjwilczynski@gmail.com>
2022-09-26 18:06:52 +0200

    update packages

--- addtional.d.ts
+++
@@ -1,7 +0,0 @@
-// We will be able to remove this file once https://github.com/sebholstein/astro-google-fonts-optimizer/pull/6 is merged and new version is published
-declare module "astro-google-fonts-optimizer" {
-  type Props = {
-    url: string | string[];
-  };
-  export const GoogleFontsOptimizer: (props: Props) => any;
-}
