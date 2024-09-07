c0926cd48abd802edad018489a6cf72a36efc5ef
Stanisław <sjwilczynski@gmail.com>
2024-09-07 18:33:33 +0200

    fix issue with types and update dev deps

---
+++ eslint.config.mjs
@@ -0,0 +1,63 @@
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import parser from "astro-eslint-parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: ["**/*.mdx"],
+  },
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:astro/recommended",
+    "plugin:astro/jsx-a11y-recommended"
+  ),
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+
+    rules: {
+      "no-constant-binary-expression": "error",
+    },
+  },
+  {
+    files: ["**/*.astro"],
+
+    languageOptions: {
+      parser: parser,
+      ecmaVersion: 5,
+      sourceType: "script",
+
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+    },
+
+    rules: {},
+  },
+];
