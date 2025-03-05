8e99fb32df7e405a13e1e1fca97df19ab2f45915
Stanisław <sjwilczynski@gmail.com>
2025-03-05 17:52:07 +0100

    bump astro to 5

--- astro.config.ts
+++ astro.config.ts
@@ -7,6 +7,9 @@ import purgecss from "astro-purgecss";
 // https://astro.build/config
 export default defineConfig({
   site: "https://sjwilczynski.github.io",
+  build: {
+    inlineStylesheets: "never",
+  },
   integrations: [
     react(),
     mdx(),
